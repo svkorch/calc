5563954287b664990e92061b7280425ad4883b3f
SK <sk@sk.sk>
2023-06-05 19:35:39 +0300

    Наполнить демонстрационную программу калькулятор примерами

--- cmd/main.go
+++ cmd/main.go
@@ -13,9 +13,13 @@ func main() {
 	f("VI / III")
 	f("VII / III")
 	f("I + II")
+	f("3 + 8")
+	f("6 - 9")
 
 	// Norman
 	f("I - II")
+	f("X - X")
+	f("III - IV")
 
 	//Bad
 	f("I + 1")
@@ -23,8 +27,9 @@ func main() {
 	f("1 + 1 + 1")
 	f("3 & 5")
 	f("3 / 55")
+	f("ZVO / 1")
 }
 
 func f(s string) {
-	fmt.Println(s + " = " + calc.Calculate(s))
+	fmt.Println(s + " = " + calc.Calculate(s) + "\n")
 }
